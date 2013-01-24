76e85043ac0347f116bf88d3aa3450752c257fd0
Zach Wagner <z.wagner@sixredmarbles.com>
2013-01-23 21:24:54 -0500

    Initial commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9fba82b7fa0e7aac174a0b17de47efb8b8d3a1dcef7afcc47544eb7f6988de519afe1802e23669257a16ea47f42bfb580cc87ce3d21befa0630a9fc86cd7819e'
